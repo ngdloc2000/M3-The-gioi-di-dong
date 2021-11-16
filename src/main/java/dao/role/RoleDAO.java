5cdbee1f6bdd197480fb3e70660aa36b9143c46a
an.ngo <phowflowf135@gmail.com>
2021-11-16 17:13:03 +0700

    try to fix confix error

--- src/main/java/dao/role/RoleDAO.java
+++ src/main/java/dao/role/RoleDAO.java
@@ -1,6 +1,10 @@
 package dao.role;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+import config.Config;
+>>>>>>> fde021ca9380dcd896fbda1193945ab52658fca4
 import model.Role;
 
 import java.sql.*;
@@ -9,15 +13,16 @@ import java.util.List;
 
 public class RoleDAO implements IRole {
     private static final String SELECT_ALL_ROLES = "select * from role";
+    private static final String SELECT_ROLE_BY_ID = "select * from role where idRole = ?";
 
     public Connection getConnection() {
         Connection connection = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName(Config.CLASS_FOR_NAME);
             connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/casestudym3",
-                    "root",
-                    "ngdloc1532k");
+                    Config.MYSQL,
+                    Config.USERNAME,
+                    Config.PASSWORD);
             System.out.println("ket noi thanh cong");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
@@ -84,7 +89,20 @@ public class RoleDAO implements IDao<Role> {
 
     @Override
     public Role findById(int id) {
-        return null;
+        Connection connection = getConnection();
+        Role role = null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ROLE_BY_ID);
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                String name = rs.getString("nameRole");
+                role = new Role(id, name);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return role;
     }
 
     @Override
