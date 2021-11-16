9b5cfe9f1682c8f8a20f9c3284b269d8f396e511
ngdloc2000 <ngdinhloc2000@gmail.com>
2021-11-16 16:24:35 +0700

    sua db

--- src/main/java/controller/AccountServlet.java
+++ src/main/java/controller/AccountServlet.java
@@ -1,15 +1,43 @@
 package controller;
 
+import dao.role.IRole;
+import dao.role.RoleDAO;
+import model.Role;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "AccountServlet", value = "/accounts")
 public class AccountServlet extends HttpServlet {
+    IRole iRole = new RoleDAO();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+                case "show":
+                    break;
+                default:
+                    listRole(request, response);
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    private void listRole(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Role> roleList = iRole.findAll();
+        request.setAttribute("list", roleList);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("account/test.jsp");
+        dispatcher.forward(request, response);
     }
 
     @Override
--- src/main/java/dao/role/RoleDAO.java
+++ src/main/java/dao/role/RoleDAO.java
@@ -1,16 +1,20 @@
 package dao.role;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import model.Role;
 
-public class RoleDAO {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoleDAO implements IRole {
+    private static final String SELECT_ALL_ROLES = "select * from role";
+
     public Connection getConnection() {
         Connection connection = null;
         try {
-            Class.forName("JDBC_DATABASE_DRIVER");
+            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/blog",
+                    "jdbc:mysql://localhost:3306/casestudym3",
                     "root",
                     "ngdloc1532k");
             System.out.println("ket noi thanh cong");
@@ -19,5 +23,43 @@ public class RoleDAO {
             System.out.println("Ket noi khong thanh cong");
         }
         return connection;
+    }
+
+    @Override
+    public List<Role> findAll() {
+        Connection connection = getConnection();
+        List<Role> roleList = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_ROLES);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("idRole");
+                String name = rs.getString("nameRole");
+                roleList.add(new Role(id, name));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return roleList;
+    }
+
+    @Override
+    public void add(Role role) {
+
+    }
+
+    @Override
+    public Role findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, Role role) {
+
+    }
+
+    @Override
+    public void remove(int id) {
+
     }
 }
