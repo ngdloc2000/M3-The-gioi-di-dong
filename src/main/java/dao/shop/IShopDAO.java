365e4a96eb0a41e4e33ea58a1f905a16e08e0b27
Your Name <uefa.euro30@gmail.com>
2021-11-17 15:01:53 +0700

    tao shop va shopDao

--- src/main/java/dao/shop/IShopDAO.java
+++ src/main/java/dao/shop/IShopDAO.java
@@ -3,5 +3,8 @@ package dao.shop;
 import dao.IDao;
 import model.Shop;
 
+import java.util.List;
+
 public interface IShopDAO extends IDao<Shop> {
+    List<Shop> findAllShopByIdUser(int idUser);
 }
