39009a956f5e5838a79b77cde3e69ae412c84a4b
An <phowflowf135@gmail.com>
2021-11-17 09:17:30 +0700

    add CartDetail Model

---
+++ src/main/java/dao/cartDetail/CartDAO.java
@@ -0,0 +1,32 @@
+package dao.cartDetail;
+
+import model.CartDetail;
+
+import java.util.List;
+
+public class CartDAO implements ICartDAO{
+    @Override
+    public List<CartDetail> findAll() {
+        return null;
+    }
+
+    @Override
+    public void add(CartDetail cartDetail) {
+
+    }
+
+    @Override
+    public CartDetail findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, CartDetail cartDetail) {
+
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+}
