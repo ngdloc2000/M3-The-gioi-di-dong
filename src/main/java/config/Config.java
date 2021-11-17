d92ef8e5d8204171a5bee51312922d20681e518a
an-ngo <88679901+an-ngo@users.noreply.github.com>
2021-11-17 09:18:42 +0700

    Delete src/main/java/config directory

--- src/main/java/config/Config.java
+++
@@ -1,8 +0,0 @@
-package config;
-
-public class Config {
-    public static final String CLASS_FOR_NAME = "com.mysql.jdbc.Driver";
-    public static final String MYSQL = "jdbc:mysql://localhost:3306/casestudym3";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "12345678";
-}
